6d2f0d9348e531a3d91a4e436e7a8707269d4a0b
Alexey Dogadaev <dogadaev142@gmail.com>
2025-08-28 14:36:35 +0300

    leetcode

--- leetcode.js
+++ leetcode.js
@@ -22,3 +22,18 @@ let maximum69Number = function (num) {
   }
   return Number(array.join(''))
 }
+
+// 3. Robot Return to Origin
+var judgeCircle = function (moves) {
+  let l = 0,
+    r = 0,
+    u = 0,
+    d = 0
+  for (let move of moves) {
+    if (move === 'L') l++
+    else if (move === 'R') r++
+    else if (move === 'U') u++
+    else if (move === 'D') d++
+  }
+  return l === r && u === d
+}
