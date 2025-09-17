f7ba45053bbedf6921194bd2e77eafe4d24f2ab9
Alexey Dogadaev <dogadaev142@gmail.com>
2025-09-17 22:22:41 +0300

    leetcode

--- leetcode.js
+++ leetcode.js
@@ -390,3 +390,18 @@ var sortList = function (head) {
 
   return head
 }
+// 23. Pascal's Triangle
+var generate = function (numRows) {
+  let arr = []
+  for (let i = 0; i < numRows; i++) {
+    arr[i] = []
+    for (let j = 0; j <= i; j++) {
+      if (j === 0 || j === i) {
+        arr[i][j] = 1
+      } else {
+        arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1]
+      }
+    }
+  }
+  return arr
+}
