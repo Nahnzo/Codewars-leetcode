dce7ab357278b45f1c1018e680fe9e70b879287c
Alexey Dogadaev <dogadaev142@gmail.com>
2025-09-21 15:10:25 +0300

    leetcode

--- leetcode.js
+++ leetcode.js
@@ -405,3 +405,13 @@ var generate = function (numRows) {
   }
   return arr
 }
+
+// 24. Maximum Depth of Binary Tree
+var maxDepth = function (root) {
+  if (!root) return 0
+
+  let left = maxDepth(root.left)
+  let right = maxDepth(root.right)
+
+  return 1 + Math.max(left, right)
+}
